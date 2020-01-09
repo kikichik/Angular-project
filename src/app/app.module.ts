94932684502c256c2212a3d3f779c2355f0d43be
NGurev <Nikita.Gurev@lanit-tercom.com>
2020-01-09 13:28:38 +0300

    last version from laptop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppinglistComponent } from './shoppinglist/shoppinglist.component';
+import { RecipelistComponent } from './recipebook/recipelist/recipelist.component';
+import { RecipeitemComponent } from './recipebook/recipelist/recipeitem/recipeitem.component';
+import { RecipedetailComponent } from './recipebook/recipedetail/recipedetail.component';
+import { ShoppinglisteditComponent } from './shoppinglist/shoppinglistedit/shoppinglistedit.component';
+import { RecipebookComponent} from './recipebook/recipebook.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppinglistComponent,
+    RecipelistComponent,
+    RecipeitemComponent,
+    RecipedetailComponent,
+    ShoppinglisteditComponent,
+    RecipebookComponent
   ],
   imports: [
     BrowserModule,
